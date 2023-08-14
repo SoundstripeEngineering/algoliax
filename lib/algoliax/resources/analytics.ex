27832197af49839fe3bfaca0543d94bf4da2a8f4
Kyle Boe <kyle@boe.codes>
2023-08-14 08:45:38 -0700

    flesh out analytics resource

--- lib/algoliax/resources/analytics.ex
+++ lib/algoliax/resources/analytics.ex
@@ -1,130 +1,166 @@
 defmodule Algoliax.Resources.Analytics do
-  import Algoliax.Utils, only: [index_name: 2, camelize: 1, format_queries: 1]
+  import Algoliax.Utils, only: [index_name: 2, camelize: 1]
   import Algoliax.Client, only: [request: 1]
 
-  def analytics_status do
+  def analytics_status(module, settings) do
+    index = index_name(module, settings)
+
     request(%{
       action: :analytics_status,
-      url_params: []
+      url_params: [index: index]
     })
   end
 
-  def top_searches do
+  def top_searches(module, settings, url_params \\ []) do
+    index = index_name(module, settings)
+
     request(%{
       action: :top_searches,
-      url_params: []
+      url_params: Keyword.merge([index: index], url_params)
     })
   end
 
-  def count_searches do
+  def count_searches(module, settings, url_params \\ []) do
+    index = index_name(module, settings)
+
     request(%{
       action: :count_searches,
-      url_params: []
+      url_params: Keyword.merge([index: index], url_params)
     })
   end
 
-  def no_results do
+  def no_results(module, settings, url_params \\ []) do
+    index = index_name(module, settings)
+
     request(%{
       action: :no_results,
-      url_params: []
+      url_params: Keyword.merge([index: index], url_params)
     })
   end
 
-  def no_clicks do
+  def no_clicks(module, settings, url_params \\ []) do
+    index = index_name(module, settings)
+
     request(%{
       action: :no_clicks,
-      url_params: []
+      url_params: Keyword.merge([index: index], url_params)
     })
   end
 
-  def no_result_rate do
+  def no_result_rate(module, settings, url_params \\ []) do
+    index = index_name(module, settings)
+
     request(%{
       action: :no_result_rate,
-      url_params: []
+      url_params: Keyword.merge([index: index], url_params)
     })
   end
 
-  def no_click_rate do
+  def no_click_rate(module, settings, url_params \\ []) do
+    index = index_name(module, settings)
+
     request(%{
       action: :no_click_rate,
-      url_params: []
+      url_params: Keyword.merge([index: index], url_params)
     })
   end
 
-  def top_hits do
+  def top_hits(module, settings, url_params \\ []) do
+    index = index_name(module, settings)
+
     request(%{
       action: :top_hits,
-      url_params: []
+      url_params: Keyword.merge([index: index], url_params)
     })
   end
 
-  def count_users do
+  def count_users(module, settings, url_params \\ []) do
+    index = index_name(module, settings)
+
     request(%{
       action: :count_users,
-      url_params: []
+      url_params: Keyword.merge([index: index], url_params)
     })
   end
 
-  def top_filters do
+  def top_filters(module, settings, url_params \\ []) do
+    index = index_name(module, settings)
+
     request(%{
       action: :top_filters,
-      url_params: []
+      url_params: Keyword.merge([index: index], url_params)
     })
   end
 
-  def top_filters_no_results do
+  def top_filters_no_results(module, settings, url_params \\ []) do
+    index = index_name(module, settings)
+
     request(%{
       action: :top_filters_no_results,
-      url_params: []
+      url_params: Keyword.merge([index: index], url_params)
     })
   end
 
-  def top_filters_for_attributes do
+  def top_filters_for_attributes(module, settings, url_params \\ []) do
+    index = index_name(module, settings)
+
     request(%{
       action: :top_filters_for_attributes,
-      url_params: []
+      url_params: Keyword.merge([index: index], url_params)
     })
   end
 
-  def top_filters_for_attribute do
+  def top_filters_for_attribute(module, settings, url_params \\ []) do
+    index = index_name(module, settings)
+
     request(%{
       action: :top_filters_for_attribute,
-      url_params: []
+      url_params: Keyword.merge([index: index], url_params)
     })
   end
 
-  def top_countries do
+  def top_countries(module, settings, url_params \\ []) do
+    index = index_name(module, settings)
+
     request(%{
       action: :top_countries,
-      url_params: []
+      url_params: Keyword.merge([index: index], url_params)
     })
   end
 
-  def average_click_position do
+  def average_click_position(module, settings, url_params \\ []) do
+    index = index_name(module, settings)
+
     request(%{
       action: :average_click_position,
-      url_params: []
+      url_params: Keyword.merge([index: index], url_params)
     })
   end
 
-  def click_positions do
+  def click_positions(module, settings, url_params \\ []) do
+    index = index_name(module, settings)
+
     request(%{
       action: :click_positions,
-      url_params: []
+      url_params: Keyword.merge([index: index], url_params)
     })
   end
 
-  def click_through_rate do
+  def click_through_rate(module, settings, url_params \\ []) do
+    index = index_name(module, settings)
+
     request(%{
       action: :click_through_rate,
-      url_params: []
+      url_params: Keyword.merge([index: index], url_params)
     })
   end
 
-  def conversion_rate do
+  def conversion_rate(module, settings, url_params \\ []) do
+    index = index_name(module, settings)
+
     request(%{
       action: :conversion_rate,
-      url_params: []
+      url_params: Keyword.merge([index: index], url_params)
     })
   end
 end
