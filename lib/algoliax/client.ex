33b1bb2175959be5d37c8c9eb22f8b974fd68bef
Kyle Boe <kyle@boe.codes>
2023-08-14 08:47:13 -0700

    WIP use Req for HTTP requests and adjust response building accordingly

--- lib/algoliax/client.ex
+++ lib/algoliax/client.ex
@@ -16,27 +16,38 @@ defmodule Algoliax.Client do
     {method, url} = Routes.url(action, url_params, retry)
     log(action, method, url, body)
 
-    method
-    |> :hackney.request(url, request_headers(), Jason.encode!(body), [
-      :with_body,
-      recv_timeout: recv_timeout()
-    ])
-    |> case do
-      {:ok, code, _headers, response} when code in 200..299 ->
-        build_response(response, request)
+    Req.new(
+      method: method,
+      url: url,
+      json: body,
+      params: url_params,
+      connect_options: [timeout: recv_timeout()]
+    )
+    |> Req.Request.put_headers(request_headers())
+    |> Req.Request.run_request()
+    |> build_response()
 
-      {:ok, code, _, response} when code in 300..499 ->
-        handle_error(code, response, action)
+    # method
+    # |> :hackney.request(url, request_headers(), Jason.encode!(body), [
+    #   :with_body,
+    #   recv_timeout: recv_timeout()
+    # ])
+    # |> case do
+    #   {:ok, code, _headers, response} when code in 200..299 ->
+    #     build_response(response, request)
 
-      error ->
-        Logger.debug("#{inspect(error)}")
-        request(request, retry + 1)
-    end
+    #   {:ok, code, _, response} when code in 300..499 ->
+    #     handle_error(code, response, action)
+
+    #   error ->
+    #     Logger.debug("#{inspect(error)}")
+    #     request(request, retry + 1)
+    # end
   end
 
-  defp handle_error(404, response, action) when action in [:get_settings, :get_object] do
-    {:error, 404, response}
-  end
+  # defp handle_error(404, response, action) when action in [:get_settings, :get_object] do
+  #   {:error, 404, response}
+  # end
 
   defp handle_error(code, response, _action) do
     error =
@@ -48,16 +59,18 @@ defmodule Algoliax.Client do
     raise Algoliax.AlgoliaApiError, %{code: code, error: error}
   end
 
-  defp build_response(response, request) do
-    case Jason.decode(response) do
-      {:ok, response} -> Algoliax.Response.new(response, request[:url_params])
-      error -> error
-    end
+  defp build_response({%{options: options}, %{status: status} = response})
+       when status in 200..299 do
+    Algoliax.Response.new(response, options)
+  end
+
+  defp build_response({%{options: options}, %{status: status} = response})
+       when status in 300..499 do
+    Algoliax.Response.new(response, options)
   end
 
   defp request_headers do
     [
-      {"Content-type", "application/json"},
       {"X-Algolia-API-Key", Config.api_key()},
       {"X-Algolia-Application-Id", Config.application_id()}
     ]
